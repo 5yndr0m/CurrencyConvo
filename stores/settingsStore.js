ec503721eb4a1ae3b5f095b76a02433ae0c6e2b3
5yndr0m <sdilanjana21@gmail.com>
2024-12-23 12:47:31 +0530

    feat: removing unwanted files

--- stores/settingsStore.js
+++
@@ -1,9 +0,0 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-export const setApiKey = async (key) => {
-  await AsyncStorage.setItem('apiKey', key);
-};
-
-export const getApiKey = async () => {
-  return await AsyncStorage.getItem('apiKey');
-};
