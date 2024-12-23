df5c21d790a06aa128e1940cd24003f5eead1620
5yndr0m <sdilanjana21@gmail.com>
2024-12-23 12:58:37 +0530

    feat: adding currency picker component

---
+++ components/CurrencyPicker.js
@@ -0,0 +1,10 @@
+// components/CurrencyPicker.js
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Menu, Button } from 'react-native-paper';
+const CurrencyPicker = ({ visible, onDismiss, onSelect }) => {
+  return (
+  );
+};
+const styles = StyleSheet.create({});
+export default CurrencyPicker;
