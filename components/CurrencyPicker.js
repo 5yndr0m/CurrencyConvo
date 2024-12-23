c7c2206cbfeb7a263cd075f765402791e2bb310d
5yndr0m <sdilanjana21@gmail.com>
2024-12-23 12:59:15 +0530

    feat: adding mock currencies

--- components/CurrencyPicker.js
+++ components/CurrencyPicker.js
@@ -2,6 +2,12 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Menu, Button } from 'react-native-paper';
+
+const currencies = [
+  'USD', 'EUR', 'GBP', 'INR', 'AUD', 'CAD', 'SGD', 'JPY', 'CNY', 'HKD', 
+  'NZD', 'ZAR', 'BRL', 'RUB', 'MXN', 'SEK', 'NOK', 'DKK', 'CHF', 'LKR'
+];
+
 const CurrencyPicker = ({ visible, onDismiss, onSelect }) => {
   return (
   );
